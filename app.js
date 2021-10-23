e9db91298c462e36296cdd5446728f6b54064803
HubCrag <hubcrag@gmail.com>
2021-10-23 10:22:40 +0300

    full code first

---
+++ app.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll('.animated-button');
+buttons.forEach(btn => {
+  btn.addEventListener('mouseenter', function(e){
+    
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+    
+    let ripples = document.createElement('span');
+    ripples.style.left = x +'px';
+    ripples.style.top = y +'px';
+    this.appendChild(ripples);
+    
+    setTimeout(() =>{
+      ripples.remove()
+    }, 1000);
+  })
+})
